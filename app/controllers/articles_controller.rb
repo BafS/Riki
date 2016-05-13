28f41a59188043887d9d7ed7a1d16eb0f4d612d7
BafS <fabacrans@gmail.com>
2016-05-13 13:43:02 +0200

    Add show, create and redirection if needed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,30 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find_by title: params[:id]
+
+    # redirect to 'new' if article is not found
+    if @article == nil
+      redirect_to action: 'new', title: params[:id]
+    end
+    # @article = Article.find(params[:id])
+  end
+
   def new
   end
 
+  def edit
+  end
+
   def create
-    # TODO
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
